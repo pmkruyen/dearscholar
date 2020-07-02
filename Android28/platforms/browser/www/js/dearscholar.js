b962e32c25e9c501dabc1b5b70ee162d2e1ad38e
pmkruyen <p.m.kruyen@gmail.com>
2020-07-02 21:22:37 +0200

    fourth commit

--- Android28/platforms/browser/www/js/dearscholar.js
+++ Android28/platforms/browser/www/js/dearscholar.js
@@ -255,7 +255,14 @@ routes: [
     {
         name: 'about',
         path: '/about/',
-        url: './about.html'
+        url: './about.html',
+        on: {
+        pageInit: function (e, page) {
+                $$('#consentAboutButton').on('click', function (e) {
+                    startConsentAbout()
+                });
+        }
+        }
     },
 ],
 // ... other parameters  
@@ -301,8 +308,8 @@ window.addEventListener("keyboardDidHide", function(e) {
     cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
 });
 
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////
-// Login functions
+///////////////////////////////////////////////////////////////////////////////
+///// Specific ANDROID functions
 
 // Timer functions to logout (return to index) after # time of inactivity.
 var timeoutID;
@@ -322,6 +329,8 @@ $$(window).on('click keyup', function (e) {
     window.clearTimeout(timeoutID);
     startTimer();
 });
+
+///////////////////////////////////////////////////////////////////////////////
 
 //Login procedure if touchID is enabled
 function loginTOUCH(){
@@ -487,6 +496,7 @@ function authentication(){
                             window.localStorage.setItem('project', settings[0].data.project);
                             window.localStorage.setItem('q0_startdate', settings[0].data.q0_startdate);
                             window.localStorage.setItem('q0_occasions', settings[0].data.q0_occasions);
+                            window.localStorage.setItem('q0_intervaltype', settings[0].data.q0_intervaltype);
                             window.localStorage.setItem('q0_interval', settings[0].data.q0_interval);
                      
                             // register the device to receive notifications
@@ -549,6 +559,11 @@ $$('#acceptConsent').on('click', function (e) {
         })   
 })
 
+//Add event handler to the close consent link in the About button.
+$$('#closeConsent').on('click', function (e) {
+    app.popup.close(".consentAbout")
+})
+
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // Database functions needed to setup the diary database and all tables
     // Wait for Phonegap to load
@@ -582,8 +597,7 @@ $$('#acceptConsent').on('click', function (e) {
                  {  
                     surveyStructure = JSON.parse(data).data0;
                     pageStructure = JSON.parse(data).data1;
-                    questionTable = JSON.parse(data).data2;
-                                         
+                    questionTable = JSON.parse(data).data2;                                         
                     DiaryDatabase.transaction(function (tx){
                         tx.executeSql('DROP TABLE IF EXISTS surveyStructure');
                         tx.executeSql('DROP TABLE IF EXISTS pageStructure');
@@ -592,7 +606,7 @@ $$('#acceptConsent').on('click', function (e) {
                         tx.executeSql('CREATE TABLE IF NOT EXISTS surveyInformation (surveydate,A,B,C,D)');
                         tx.executeSql('CREATE TABLE IF NOT EXISTS surveyStructure (consent, manual, moduleAname, moduleBname, moduleCname, moduleDname)');
                         tx.executeSql('CREATE TABLE IF NOT EXISTS pageStructure (module1, classp, idp, header, backbuttontid, backbuttontext, backbuttononclick, backbuttonhref, nextbuttonid, nextbuttontext, nextbuttononclick,nextbuttonhref)');
-                        tx.executeSql('CREATE TABLE IF NOT EXISTS questiontable (module2, tab, idq, type, question, footer)');
+                        tx.executeSql('CREATE TABLE IF NOT EXISTS questiontable (module2, tab, idq, type, question, categories, footer)');
                         tx.executeSql('CREATE TABLE IF NOT EXISTS responseTable (id, surveydate, timestamp)');
                         
                         DiaryDatabase.transaction(function(tx) {
@@ -623,7 +637,8 @@ $$('#acceptConsent').on('click', function (e) {
                                                  questionTable[i].data.tab, 
                                                  questionTable[i].data.idq, 
                                                  questionTable[i].data.type, 
-                                                 questionTable[i].data.question, 
+                                                 questionTable[i].data.question,
+                                                 questionTable[i].data.categories,
                                                  questionTable[i].data.footer
                                                  );
                             listQuestionIds.push(questionTable[i].data.idq);
@@ -685,27 +700,35 @@ $$('#acceptConsent').on('click', function (e) {
     }
 
     // function to populate the questiontable database
-    function addItemQuestiontable(DiaryDatabase, module2, tab, idq, type, question, footer) {
+    function addItemQuestiontable(DiaryDatabase, module2, tab, idq, type, question, categories, footer) {
 
         DiaryDatabase.transaction(function (tx) {
 
-        var query = "INSERT INTO questiontable (module2, tab, idq, type, question, footer) VALUES (?,?,?,?,?,?)";
+        var query = "INSERT INTO questiontable (module2, tab, idq, type, question, categories, footer) VALUES (?,?,?,?,?,?,?)";
 
-        tx.executeSql(query, [module2, tab, idq, type, question, footer], function(tx) {
+        tx.executeSql(query, [module2, tab, idq, type, question, categories, footer], function(tx) {
         });
     });
     }
 
 // function to create the survey schedule
 
-function measurementDates (startdate=window.localStorage.getItem("q0_startdate"), occassions = window.localStorage.getItem('q0_occasions'), interval = window.localStorage.getItem("q0_interval")){
+function measurementDates (startdate=window.localStorage.getItem("q0_startdate"), occassions = window.localStorage.getItem('q0_occasions'),intervaltype = window.localStorage.getItem("q0_intervaltype"), interval = window.localStorage.getItem("q0_interval")){
+    
+    
     
     var currentmoment =moment();
         
     for (i = 0; i < Number(occassions)*Number(interval); i = i + Number(interval)) {
         
-        var measurementOccassion = moment(startdate).add(i, 'weeks');
-
+        if (intervaltype=='d'){
+            var measurementOccassion = moment(startdate).add(i, 'days');
+        }
+        
+        if (intervaltype=='w'){
+            var measurementOccassion = moment(startdate).add(i, 'weeks');
+        }
+        
         month = measurementOccassion.format('MMM');
         day = measurementOccassion.format('DD');
         year = measurementOccassion.format('YYYY');
@@ -769,6 +792,26 @@ function measurementDates (startdate=window.localStorage.getItem("q0_startdate")
       function (tx, succes) {
          app.popup.open(".consent");
          document.getElementById("emptyconsentform").innerHTML += consentText   
+     })
+ }
+
+// function to populate the consent page in the About page
+ function startConsentAbout(){
+      document.getElementById("emptyconsentformAbout").innerHTML = ""   
+
+      DiaryDatabase.transaction(function (tx) {
+            
+                var query = "SELECT consent FROM surveyStructure WHERE rowid = ?"
+                tx.executeSql(query,[1],function (tx, resultSet) {
+                    consentText = resultSet.rows.item(0).consent
+                })
+      },
+      function (tx, error) {
+        app.dialog.alert("Er is iets mis gegaan. Probeer opnieuw of neem contact op met Peter Kruyen.","DearScholar")
+      },
+      function (tx, succes) {
+         app.popup.open(".consentAbout");
+         document.getElementById("emptyconsentformAbout").innerHTML += consentText   
      })
  }
 
@@ -904,6 +947,7 @@ function startModule(module, adhoc) {
                             tab: resultSet.rows.item(x).tab,
                             idq: resultSet.rows.item(x).idq,
                             question: resultSet.rows.item(x).question,
+                            categories: resultSet.rows.item(x).categories,
                             footer: resultSet.rows.item(x).footer
                         }
                         
@@ -917,6 +961,7 @@ function startModule(module, adhoc) {
                         }
                         if(resultSet.rows.item(x).type=="MC"){
                             emptytab.innerHTML += multiplechoicequestion(data);
+                            multiplechoicequestionselect(data);
                         }
                         if(resultSet.rows.item(x).type=="SL"){
                             emptytab.innerHTML += sliderquestion(data);
@@ -979,7 +1024,7 @@ function startModule(module, adhoc) {
     }
 
     // layout function for multiple choice questions
-    function multiplechoicequestion(data){
+    function multiplechoicequestion(data){        
         return `
         <form class="form-store-data" id=${data.tab}> 
             <div class="block block-strong no-hairlines">
@@ -989,17 +1034,26 @@ function startModule(module, adhoc) {
             <div class="item-input-wrap input-dropdown-wrap">
             <select class="question" style="width:100%" name=${data.idq} id=${data.idq} placeholder="">
                 <option disabled selected value> -- Kies een antwoord -- </option>
-                <option value="ZO">Zeer onwaarschijnlijk</option>
-                <option value="O">Onwaarschijnlijk</option>
-                <option value="NE">Neutraal</option>
-                <option value="WA">Waarschijnlijk</option>
-                <option value="ZW">Zeer waarschijnlijk</option>
             </select>
             </div>
             </div>
             <div class="block-footer">${data.footer}</div>  
             </div>
-        </form> `
+        </form>`
+    }
+
+    // layout function for the options in the multiple choice questions
+    function multiplechoicequestionselect(data){ 
+        select = document.getElementById(data.idq)
+        
+        optionValues = Object.getOwnPropertyNames(JSON.parse(data.categories))
+        
+        for (i = 0; i < optionValues.length; i++) {
+            option = document.createElement("option")
+            option.value =  optionValues[i]
+            option.text = Object(JSON.parse(data.categories))[optionValues[i]]
+            select.appendChild(option)
+        }
     }
 
     // layout function for the slider questions
