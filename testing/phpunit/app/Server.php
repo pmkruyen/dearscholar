e28c367c15c55eea1618e1ab02d503089ee6322f
pmkruyen <p.m.kruyen@gmail.com>
2020-08-17 19:19:08 +0200

    update docker for phpunit

--- testing/phpunit/app/Server.php
+++ testing/phpunit/app/Server.php
@@ -17,7 +17,7 @@ class Server {
     $this->options = $options;
   }
   public function post_server() {
-    $client = new \GuzzleHttp\Client(["base_uri" => "http://localhost:80"]);
+    $client = new \GuzzleHttp\Client(["base_uri" => "0.0.0.0:80"]);
     $response = $client->post("/dearscholar.php", $this->options);
     return $response->getBody();
   }
